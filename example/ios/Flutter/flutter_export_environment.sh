7002dea6769ef2fa9e0edc8af810a9dc520d2246
himanshu-akasa <himanshu.kumawat@akasaair.com>
2024-12-17 16:42:58 +0530

    Update version

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/tungakanui/fvm/versions/3.13.4"
-export "FLUTTER_APPLICATION_PATH=/Users/tungakanui/development/flutter-alice/example"
+export "FLUTTER_ROOT=/Users/Himanshukumawat/Desktop/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Himanshukumawat/Desktop/plugin/flutter-alice/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
